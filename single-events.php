b0f54f0f29385d285ca454585ce94e6d3f09eb5e
Teekkoo <genuino1617@gmail.com>
2021-04-13 16:01:54 -0300

    muda tamanho de fonte do texto da single-events

--- single-events.php
+++ single-events.php
@@ -11,15 +11,18 @@
     if(have_posts()): while(have_posts()): the_post();
 ?>
 <section class="section-info container d-flex">
-    <div class="info-content">
-        <h1 class="info-principal-title txt-cent"><?php the_title() ?></h1>
-        <div class="my-content text-prin txt-justify">
-            <div class="thumb-noticias">
-                <?php the_post_thumbnail()?>
-            </div>
-            <?php the_content()?>
-        </div>
-    </div>
+<div class="content-news">
+		<h1 class="info-principal-title txt-cent"><?php the_title(); ?></h1>
+		<p class="news-date">Publicado em: <?php echo get_the_date('', $post->ID);?></p>
+		<div class="my-content text-prin txt-justify">
+			<div class="thumb-noticias">
+			<?php the_post_thumbnail();?>
+			</div>
+			<div class="info-txt">
+				<?php the_content();?>
+			</div>
+		</div>
+	</div>
 </section>
 <?php 
     endwhile; endif
